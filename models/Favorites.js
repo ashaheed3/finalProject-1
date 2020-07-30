d6fa248932377da0a65b86cd2e54a66c7c68f225
clemson tigress <anreeves83@gmail.com>
2020-07-29 22:09:54 -0400

    added card layout to favorite page

--- models/Favorites.js
+++ models/Favorites.js
@@ -6,20 +6,18 @@ const favoritesSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  // url string for thumbnail image
+
   thumbnail: {
     type: String,
     default: '',
   },
-  // url for recipe web page - unique index
+
   href: {
     type: String,
     default: '',
     unique: true,
   },
 
-  // Not all ingredients, just the recommended ingredients from scraped web pages
-  // from which seed data was sourced
   ingredients: [String],
 });
 
