58e33958c1f2c26af84a00eb2f42d0c565de8aba
Dom W <domiwar.it@gmail.com>
2020-08-10 14:07:20 -0400

    pref buttons

---
+++ client/src/components/Buttons/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import TheGrid from './grid'
+
+export default class AllButtons extends React.Component {
+    render(){
+        return (
+            <div>
+                <TheGrid/>
+            </div>
+        )
+    }
+}
