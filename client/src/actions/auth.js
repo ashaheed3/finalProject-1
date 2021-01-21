20f6e2bf6b5923ed98197283d7b744ad2f822f9b
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2021-01-20 23:17:35 -0500

    login function posting to the wrong route. Corrected

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -73,7 +73,7 @@ export const login = (username, password) => async dispatch => {
     const body = JSON.stringify({username, password});
 
     try{
-        const res = await axios.post('api/auth', body, config);
+        const res = await axios.post('api/users', body, config);
 
         dispatch({
             type: LOGIN_SUCCESS,
